edb842d18cb842cedd5917409e44120ef7516081
Francivaldo.Alves <francivaldo.alves@gmail.com>
2020-07-07 19:37:09 -0300

    add .env

--- src/database.js
+++ src/database.js
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose';
+require('dotenv').config();
+
 //conexao com banco de dados
+
 class Database {
   constructor() {
     this.init();
   }
   init() {
     mongoose.connect(
-      'mongodb://localhost:27017/my-api',
+      process.env.MONGO_URI,
+      //'mongodb://localhost:27017/my-api',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
